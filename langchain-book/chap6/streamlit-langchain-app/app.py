17e2cdcb9e732ed758354b2b1a1f43f234bd5756
tkhashi <pupodenews@gmail.com>
2024-11-27 20:22:40 +0900

    6-6

--- langchain-book/chap6/streamlit-langchain-app/app.py
+++ langchain-book/chap6/streamlit-langchain-app/app.py
@@ -1,13 +1,30 @@
+import os
 import streamlit as st
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+load_dotenv()
 
 st.title("langchain!")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 prompt = st.chat_input("What's up?")
-print(prompt)
 
 if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("user"):
         st.markdown(prompt)
+
     with st.chat_message("assistant"):
-        res = "こんちは"
-        st.markdown(res)
+        response = "こんちはー"
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
